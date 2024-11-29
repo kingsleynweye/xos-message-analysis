ec1f91ef26cb1fdf914647eea6317d1ba23132c2
Kingsley Nweye <etonwana@yahoo.com>
2024-11-29 11:40:55 -0600

    removed pyyaml dependency

--- src/utilities.py
+++ src/utilities.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-import yaml
 
 class FileHandler:
     ROOT_DIRECTORY = os.path.join(*Path(os.path.dirname(os.path.abspath(__file__))).parts[0:-1])
@@ -14,46 +13,6 @@ class FileHandler:
     CREATE_INDEXES_QUERY_FILEPATH = os.path.join(QUERIES_DIRECTORY, 'create_indexes.sql')
     INSERT_PRELIMINARY_QUERY_FILEPATH = os.path.join(QUERIES_DIRECTORY, 'insert_preliminary.sql')
     INSERT_DASHBOARD_MESSAGES_QUERY_FILEPATH = os.path.join(QUERIES_DIRECTORY, 'insert_dashboard_messages.sql')
-    
-    def read_yaml(filepath: str) -> dict:
-        """Return YAML document as dictionary.
-        
-        Parameters
-        ----------
-        filepath : str
-        pathname of YAML document.
-        
-        Returns
-        -------
-        dict
-            YAML document converted to dictionary.
-        """
-
-        with open(filepath, 'r') as f:
-            data = yaml.safe_load(f)
-
-        return data
-
-    def write_yaml(filepath: str, dictionary: dict, **kwargs):
-        """Write dictionary to YAML file. 
-        
-        Parameters
-        ----------
-        filepath : str
-            pathname of YAML document.
-        dictionary: dict
-            dictionary to convert to YAML.
-
-        Other Parameters
-        ----------------
-        **kwargs : dict
-            Other infrequently used keyword arguments to be parsed to `yaml.safe_dump`.
-        """
-
-        kwargs = {'sort_keys': False, 'indent': 2, **kwargs}
-        
-        with open(filepath, 'w') as f:
-            yaml.safe_dump(dictionary, f, **kwargs)
 
     def get_data_from_path(filepath: Path | str) -> Path | str:
         if os.path.exists(filepath):
